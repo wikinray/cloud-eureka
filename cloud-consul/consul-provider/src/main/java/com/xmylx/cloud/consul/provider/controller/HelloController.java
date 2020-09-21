4a411914c8fd14375235cf9f97b17be319d46f99
wql <weiqinlei@jajaying.com>
2020-09-21 16:03:04 +0800

    consul config demo

--- cloud-consul/consul-provider/src/main/java/com/xmylx/cloud/consul/provider/controller/HelloController.java
+++ cloud-consul/consul-provider/src/main/java/com/xmylx/cloud/consul/provider/controller/HelloController.java
@@ -1,15 +1,22 @@
 package com.xmylx.cloud.consul.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/consul/provider")
 public class HelloController {
 
+    @Value("${consul.provider.hello.name}")
+    String message;
+
+
     @GetMapping("/hello/sayHello")
     public String sayHello(String name){
-        return "hello,"+name;
+        return "hello,"+name+" ------->"+message;
     }
 }
