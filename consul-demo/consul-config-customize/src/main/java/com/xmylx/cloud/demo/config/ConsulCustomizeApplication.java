caf4b77edfed15b69f8c1924f071393dafb0f3ab
wql <weiqinlei@jajaying.com>
2020-09-21 18:01:41 +0800

    consul demo   config

---
+++ consul-demo/consul-config-customize/src/main/java/com/xmylx/cloud/demo/config/ConsulCustomizeApplication.java
@@ -0,0 +1,14 @@
+package com.xmylx.cloud.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ConsulCustomizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulCustomizeApplication.class,args);
+    }
+}
