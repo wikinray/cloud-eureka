e3b0d1fc84e44c79be32c21ed2260a2ae547483a
wql <weiqinlei@jajaying.com>
2020-09-25 15:15:56 +0800

    cloud sleuth 自定义baggage 未成功？？？

--- cloud-sleuth/sleuth-provider/src/main/java/com/xmylx/cloud/sleuth/provider/controller/ProviderController.java
+++ cloud-sleuth/sleuth-provider/src/main/java/com/xmylx/cloud/sleuth/provider/controller/ProviderController.java
@@ -1,6 +1,5 @@
 package com.xmylx.cloud.sleuth.provider.controller;
 
-import brave.baggage.BaggageField;
 import brave.propagation.ExtraFieldPropagation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
